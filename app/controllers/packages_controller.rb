6b4a0c94d342dff0380a1b9e47e4bb8b864fca46
Ahmed El.Hussaini <aelhussaini@gmail.com>
2016-02-06 19:17:13 +0400

    finished the search controller page

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -3,5 +3,7 @@ class PackagesController < ApplicationController
   end
 
   def search
+    @q = params[:q]
+    @packages = Package.where(title: /#{@q}/).order(downloads: :desc).page(params[:page]).per(5)
   end
 end
